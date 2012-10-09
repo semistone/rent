593c19593f3dcc8b5a4690141bcc80d630472c85
semistone <semistone@gmail.com>
2012-10-09 17:23:46 +0800

    fix qunit test in mobile auth request

--- WebContent/test/backyard.test.js
+++ WebContent/test/backyard.test.js
@@ -39,9 +39,16 @@ asyncTest('test sently callback', function(assert) {
 //
 // insert into DEVICE values ('SSO','test sso','','token','5','1','','0','0');
 // insert into USER values ('test sso','234242424','','','','','','','','','1','1349743961','1349743961');
+// insert into USER values ('test list devices','23432424','test@email.com','test list devices','','','','','','','1','1349743961','1349743961');
 //
 asyncTest('test mobile auth request', function(assert) {
+	var test_device_cookie_value="B0559903B6F1A374BA0F1AEC8CBC6C6B59AC9C0799BA776136A29B67846B5B9B12ED3F2CE1E88838AAEC6289A97A16D6";
+	$.cookie("D", test_device_cookie_value,{path:'/'});
 	var data = "{\"mobilePhone\":\"+886911826844\",\"requestFrom\":\"test sso\",\"requestId\":\"test request id\",\"done\":\"http://done\"}";
+	var success = function(){
+		ok(true, 'expect execute success');
+		start();		
+	};
 	var setting = {
 	    url:'../rest/user/mobile_auth_request',
 	    data: data,
@@ -51,10 +58,7 @@ asyncTest('test mobile auth request', function(assert) {
 	    success:success
 	};
 	$.ajax(setting);
-	var success = function(){
-		ok(true, 'expect execute success');
-		start();		
-	}
+
 });
 
 
