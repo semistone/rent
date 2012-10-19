569477e7c90858cd245ff28783e33a76ef30b724
angus_chen <angus_chen@trend.com.tw>
2012-10-19 17:16:18 +0800

    rename

--- test/org/siraya/rent/user/dao/TestMobileAuthRequestDao.java
+++ test/org/siraya/rent/user/dao/TestMobileAuthRequestDao.java
@@ -1,43 +1,44 @@
 package org.siraya.rent.user.dao;
 
+import junit.framework.Assert;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.siraya.rent.pojo.MobileAuthRequest;
-import org.siraya.rent.pojo.MobileAuthResponse;
-
-@ContextConfiguration(locations = { "classpath*:/applicationContext.xml",
-		"classpath*:/applicationContext-mybatis.xml" })
-public class TestMobileAuthRequestDao extends AbstractJUnit4SpringContextTests {
-	@Autowired
-	private IMobileAuthRequestDao mobileAuthRequestDao;
-	@Autowired
-	private IMobileAuthResponseDao mobileAuthResponseDao;
-	private MobileAuthRequest request;
-	private MobileAuthResponse response;
-
-	@Before
-	public void setUp() {
-		long time=java.util.Calendar.getInstance().getTimeInMillis();
-		String requestId = "r"+time/1000;
-		request = new MobileAuthRequest();
-		request.setRequestTime(time/1000);
-		request.setForceReauth(true);
-		request.setRequestFrom("xxx");
-		request.setDone("http://www.yahoo.com");
-		request.setAuthUserId("auth id ");
-		request.setRequestId(requestId);
-		response = new MobileAuthResponse();
-		response.setRequestId(requestId);
-		response.setStatus(1);
-		response.setResponseTime(time/1000);
-	}
-
-	@Test
-	public void testCRUD() {
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+import java.util.List;
+@ContextConfiguration(locations = {"classpath*:/applicationContext.xml","classpath*:/applicationContext-mybatis.xml"})
+public class TestMobileAuthRequest extends AbstractJUnit4SpringContextTests{
+    @Autowired
+    private IMobileAuthRequestDao mobileAuthRequestDao;
+    private MobileAuthRequest request;
+    @Before
+	public void setUp(){
+    	request = new MobileAuthRequest();
+    	long time=java.util.Calendar.getInstance().getTimeInMillis();
+    	request.setRequestId("r"+time/1000);
+    	request.setForceReauth(false);
+    	request.setRequestTime(time/1000);
+    	request.setAuthUserId("test id");
+    	request.setRequestFrom("test xxx");
+    	request.setDone("http://www.yahoo.com");
+    }
+	
+    @Test   
+    public void testCRUD(){
 		mobileAuthRequestDao.newRequest(request);
-		mobileAuthResponseDao.updateResponse(response);
-	}
+		MobileAuthRequest request2 = mobileAuthRequestDao.get(request
+				.getRequestId());
+		Assert.assertEquals(request.getAuthUserId(), request2.getAuthUserId());
+
+		List<MobileAuthRequest> list = mobileAuthRequestDao
+				.getRequestsByAuthUser(request.getAuthUserId(), 10, 0);
+		Assert.assertNotSame(0, list.size());
+
+		list = mobileAuthRequestDao.getRequestsByFrom(request.getRequestFrom(),
+				10, 0);
+		Assert.assertNotSame(0, list.size());
+    }
 }
