295e4ec7666c771260d3c445b3be5bd64a1d5c4e
Angus Chen <semistone@gmail.com>
2012-11-18 23:20:29 +0800

    use page api

--- WebContent/js/modules/home/view.main.js
+++ WebContent/js/modules/home/view.main.js
@@ -5,17 +5,25 @@ define([
   'Mustache',
   'RentCommon',
   'logger',
-  'text!../../../html/home/tmpl.main.html'
-  ], function($, _, Backbone, Mustache, RENT, logger,template) {
+  'text!../../../html/home/tmpl.main.html',
+  './model.page'
+  ], function($, _, Backbone, Mustache, RENT, logger,template, PageModel) {
 var $template = $('<div>').append(template);	
 
 var MainView = Backbone.View.extend({
 	initialize : function() {
+		_.bindAll(this, 'render');
 		this.tmpl = $template.find('#tmpl_home').html();
-		this.render();
+		this.model = new PageModel();
+		this.model.on('change', this.render);
+		this.model.set({id:'home'}, {silent:true});
+		this.model.fetch();
 	},
 	render:function(){
-		this.$el.html(this.tmpl);
+		logger.debug('render');
+		var s = this.model.toJSON();
+		logger.debug(s);
+		this.$el.html(Mustache.to_html(this.tmpl, this.model.toJSON()));
 	},
 	navibar:function(el){
 		logger.debug('render navibar');
