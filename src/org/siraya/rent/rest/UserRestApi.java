86c272afeeb6fb3e37c517e3dc88bcad31608be1
Ubuntu <ubuntu@ip-10-142-55-159.ap-southeast-1.compute.internal>
2012-12-23 10:12:34 +0000

    add

---
+++ src/org/siraya/rent/rest/UserRestApi.java
@@ -0,0 +1,50 @@
+package org.siraya.rent.rest;
+
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+import java.net.HttpURLConnection;
+import java.util.Map;
+import java.util.HashMap;
+import org.siraya.rent.user.service.IUserService;
+import org.siraya.rent.user.service.ISessionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.siraya.rent.pojo.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Component("userRestApi")
+@Path("/user")
+public class UserRestApi {
+	@Autowired
+	private IUserService userService;
+	@Autowired
+	private ISessionService sessionService;
+    private static Map<String,String> OK;
+	private static Logger logger = LoggerFactory
+			.getLogger(UserRestApi.class);
+    public UserRestApi(){
+    	logger.debug("new user rest api");
+    	if (OK == null) {
+    		OK = new HashMap<String,String>();
+    		OK.put("status", "SUCCESS");
+    	}
+    }
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/online_status")
+	public List<UserOnlineStatus> onlineStatus(List<String> ids){
+        return sessionService.list(ids);
+	}
+	
+}
