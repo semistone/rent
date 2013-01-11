f9d2eed005d53de591bf8e4c1cc059788053b3e9
angus_chen <angus_chen@trend.com.tw>
2013-01-11 17:07:23 +0800

    add

---
+++ src/org/siraya/rent/pojo/Image.java
@@ -0,0 +1,49 @@
+package org.siraya.rent.pojo;
+
+public class Image {
+	private String imgTarget;
+	private String shareUrl;
+	private long created;
+	private long modified;
+	private String id;
+	private String imgGroup;
+	
+	public String getImgTarget() {
+		return imgTarget;
+	}
+	public void setImgTarget(String imgTarget) {
+		this.imgTarget = imgTarget;
+	}
+	public String getShareUrl() {
+		return shareUrl;
+	}
+	public void setShareUrl(String shareUrl) {
+		this.shareUrl = shareUrl;
+	}
+	public long getCreated() {
+		return created;
+	}
+	public void setCreated(long created) {
+		this.created = created;
+	}
+	public long getModified() {
+		return modified;
+	}
+	public void setModified(long modified) {
+		this.modified = modified;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getImgGroup() {
+		return imgGroup;
+	}
+	public void setImgGroup(String imgGroup) {
+		this.imgGroup = imgGroup;
+	}
+
+	
+}
