5e78542ae6fe4d485a8eb78626b13adcf91e0975
angus_chen <angus_chen@trend.com.tw>
2013-01-31 16:29:02 +0800

    fix test

--- test/org/siraya/rent/dropbox/dao/TestImageDao.java
+++ test/org/siraya/rent/dropbox/dao/TestImageDao.java
@@ -22,6 +22,7 @@ public class TestImageDao extends AbstractJUnit4SpringContextTests{
     	long time=java.util.Calendar.getInstance().getTimeInMillis();
     	image.setId("i"+time);
     	image.setCreated(time/1000);
+    	image.setUserId("xxxxxX");
     	image.setImgTarget("test/test1");
     	image.setShareUrl("http://hahaha.com/test.img");
     	image.setModified(time/1000);
@@ -32,7 +33,7 @@ public class TestImageDao extends AbstractJUnit4SpringContextTests{
     	int ret = test.insert(image);
     	Assert.assertEquals(1, ret);
     	
-    	ret = test.delete(image.getId());
+    	ret = test.delete(image.getId(),image.getUserId());
     	Assert.assertEquals(1, ret);
     }
 }
