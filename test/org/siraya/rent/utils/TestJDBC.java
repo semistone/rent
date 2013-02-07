bd945a3081d898f8eb9a1a4c475a009e02a11b7d
angus_chen <angus_chen@trend.com.tw>
2013-02-07 15:07:34 +0800

    start to use sqlite

--- test/org/siraya/rent/utils/TestJDBC.java
+++ test/org/siraya/rent/utils/TestJDBC.java
@@ -1,7 +1,9 @@
 package org.siraya.rent.utils;
 
 
-import java.sql.DriverManager;
+import java.sql.*;
+
+import junit.framework.Assert;
 
 import org.junit.Test;
 
@@ -22,5 +24,33 @@ public class TestJDBC {
 						+ "MySQL server using TCP/IP...");
 		}
 
+	}
+	
+	
+	@Test 
+	public void testSqlite() throws Exception{
+		java.sql.Connection connection = null;
+		try {
+			Class.forName("org.sqlite.JDBC");
+			connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
+			Statement statement = connection.createStatement();
+			statement.setQueryTimeout(30); // set timeout to 30 sec.
+
+			statement.executeUpdate("drop table if exists person");
+			statement
+					.executeUpdate("create table person (id integer, name string)");
+			statement.executeUpdate("insert into person values(1, 'leo')");
+			statement.executeUpdate("insert into person values(2, 'yui')");
+			ResultSet rs = statement.executeQuery("select * from person");
+			if (rs.next()) {
+				// read the result set
+				Assert.assertEquals("leo", rs.getString("name"));
+				Assert.assertEquals(1, rs.getInt("id"));
+			}
+		} finally {
+			if (connection != null && !connection.isClosed())
+				;
+
+		}
 	}
 }
